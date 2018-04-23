16160bc0af4ddebc9784b41a12724438c45ce3f2
SimeonGeorgiev <sgeorgievbngo@gmail.com>
2018-04-23 21:53:29 +0100

    GMM filter

--- filter_with_gmm.py
+++ filter_with_gmm.py
@@ -11,15 +11,16 @@ def filter_with_gmm(df, viability_predictors, rs=42, P=90, nc=1):
   df = dataframe containing mass cytometry data to be filtered
   viability_predictors = list of marker names AND other parameters to be filtered:
           DNA, ForwardScatter, cell length, markers etc...
-  rs = random_state; P = percentile to cut off, default is P=90
+  rs = random_state; 
+  P = percentile to cut off, default is P=90
   nc = components to use, recommended 1.
   
   Filters the dataset using the log likelihood of multivariate gaussian.
-  One component is sufficient as it seems produces the same results.
+  One component is sufficient as it seems to produce the same results.->
   When this function is ran on the same dataset with different random_state values:
   numbers = []
   for i in range(1, 200):
-    df, gmm = filter_with_gmm(df, viability_predictors, rs=i, P=90)
+    df, gmm = filter_with_gmm(df, viability_predictors, rs=i, P=90, nc=1)
     numbers.append(sum(~df['inlier']))
   
   numbers = pd.Series(numbers)
