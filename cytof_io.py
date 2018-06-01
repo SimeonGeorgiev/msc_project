4b76844d6042afde1d9b36967c44d1c31d2e9379
SimeonGeorgiev <sgeorgievbngo@gmail.com>
2018-06-01 14:07:16 +0100

    updated cytof_io

--- cytof_io.py
+++ cytof_io.py
@@ -16,6 +16,30 @@ def load_df(file="Levine_32dimtransform.csv", s_e_markers=(4, 36), seed=42, n=10
 	df.fillna(value=-1, inplace=True)
 	return shuffle(df, n), markers
 
+def rename(fcs, all_=False):
+    old_names = []
+    new_names = []
+    map_names = {}
+    for PS in filter(lambda x: "$P" in x and "S" in x, fcs.meta.keys()):
+        new_name = fcs.meta[PS]
+        new_names.append(new_name)
+        or_name = PS[2:-1]
+        old_name = fcs.meta['_channel_names_'][int(or_name)-1]
+        old_names.append(old_name)
+        map_names.update({old_name: new_name})
+
+    fcs.data = fcs.data.rename(map_names, axis='columns')
+    if all_:
+        return fcs, old_names, new_names, map_names
+    else:
+        return fcs
+
+def load_fcs(file, markers=None, n=2, scale=5, ID=None, seed=42, trans=True):
+    np.random.seed(seed)
+    if ID is None:
+        ID = file
+    return rename(FCM(ID=ID, datafile=file))
+
 if __name__ == "__main__":
 	import pandas as pd
 	df = pd.DataFrame({'A':range(10), 'B':range(10)})
