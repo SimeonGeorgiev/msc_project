0b0867357248db8f294b04da81f43743d24f20a6
SimeonGeorgiev <sgeorgievbngo@gmail.com>
2018-06-01 14:48:35 +0100

    updated cytof_io

--- cytof_io.py
+++ cytof_io.py
@@ -1,5 +1,6 @@
 from pandas import read_csv, concat
 import numpy as np
+from FlowCytometryTools import FCMeasurement as FCM
 
 def shuffle(df, n):
 	for _ in range(n):
@@ -40,12 +41,33 @@ def load_fcs(file, markers=None, n=2, scale=5, ID=None, seed=42, trans=True):
         ID = file
     return rename(FCM(ID=ID, datafile=file))
 
+def arcsinh_trans(scale=5):
+    def wrapped(X):
+        return np.arcsinh(X/scale)
+    return wrapped
+
 def get_C1_CA_metals(metals):
     not_cd45 = list(filter(lambda x: 'CD45' not in x, metals))
     not_labelled = list(filter(lambda x: '_' not in x, not_cd45))
     labelled = list(filter(lambda x: '_' in x, not_cd45))
     labels_for_live = list(filter(lambda x: ('DEAD' not in x) and ('DNA' not in x), labelled))
     return not_cd45, not_labelled, labelled, labels_for_live
+
+def load_sample(ID, metals=(2, -4)):
+    stim = load_fcs(ID="Stim", file="{} Stim Cleaned.fcs".format(ID)).data
+    unstim = load_fcs(ID="Unstim", file="{} Unstim Cleaned.fcs".format(ID)).data
+    metal_names = list(stim)[metals[0]: metals[1]]
+    trans = arcsinh_trans()
+    IDstim = trans(stim.copy()[metal_names])
+    IDunstim = trans(unstim.copy()[metal_names])
+
+    IDstim['S'] = True
+    IDstim[ID] = True
+    IDunstim['S'] = False
+    IDunstim[ID] = True
+    ID_sample_df = IDstim.append(IDunstim)
+    return ID_sample_df
+                      
 if __name__ == "__main__":
 	import pandas as pd
 	df = pd.DataFrame({'A':range(10), 'B':range(10)})
