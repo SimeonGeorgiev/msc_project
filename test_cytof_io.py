947e7ff1636c4c31b5fb00e96c0ef727b15e8d41
SimeonGeorgiev <sgeorgievbngo@gmail.com>
2018-06-02 11:54:01 +0100

    updated test_cytof_io.py

--- test_cytof_io.py
+++ test_cytof_io.py
@@ -1,5 +1,6 @@
 import os
 from cytof_io import *
+import pandas as pd
 from pandas import DataFrame
 import numpy as np
 os.chdir("PBMC_150518")
@@ -10,7 +11,7 @@ def test_loaded_sample(ID):
 	assert type(ID) == DataFrame, "Not a DataFrame"
 	assert ID.shape[1] < ID.shape[0], str(ID.shape) + ". Not of right shape."
 	assert len(ID.index.unique()) == len(ID), "Duplicate indices."
-	assert sum(np.isnan(ID.values)) == 0, "NaNs in the data."
+	assert ID.isnull().values.sum() == 0, "NaNs in the data."
 
 test_loaded_sample(CA)
 test_loaded_sample(C1)
