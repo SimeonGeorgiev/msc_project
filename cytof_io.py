d515d8a417633893f959ea5d1aebdae451a17546
Simeon Georgiev <sgeorgievbngo@gmail.com>
2018-06-14 11:57:03 +0100

    udpated cytof_io.py

--- cytof_io.py
+++ cytof_io.py
@@ -1,8 +1,9 @@
 from pandas import read_csv, concat
 import numpy as np
 from FlowCytometryTools import FCMeasurement as FCM
-
-def shuffle(df, n):
+import os
+from contextlib import contextmanager
+def shuffle(df, n=1):
 	for _ in range(n):
 		df = df.reindex(np.random.permutation(df.index))
 	return df
@@ -28,18 +29,32 @@ def rename(fcs, all_=False):
         old_name = fcs.meta['_channel_names_'][int(or_name)-1]
         old_names.append(old_name)
         map_names.update({old_name: new_name})
-
     fcs.data = fcs.data.rename(map_names, axis='columns')
     if all_:
         return fcs, old_names, new_names, map_names
     else:
         return fcs
 
-def load_fcs(file, markers=None, n=2, scale=5, ID=None, seed=42, trans=True):
-    np.random.seed(seed)
+@contextmanager
+def cd(path):
+    """Usage:
+    with cd(to_some_dir):
+        envoy.run('task do')
+    from GitHub: https://gist.github.com/svetlyak40wt/6727327
+    """
+    old_path = os.getcwd()
+    os.chdir(path)
+    yield
+    os.chdir(old_path)
+
+def load_fcs(file, scale=5, ID=None, seed=42, trans=True):
     if ID is None:
         ID = file
-    return rename(FCM(ID=ID, datafile=file))
+    df = rename(FCM(ID=ID, datafile=file)).data
+    if trans:
+        return arcsinh_trans(scale)(df)
+    else:
+        return df
 
 def arcsinh_trans(scale=5):
     def wrapped(X):
@@ -47,19 +62,21 @@ def arcsinh_trans(scale=5):
     return wrapped
 
 def get_C1_CA_metals(metals):
+    cd_45 = list(filter(lambda x: 'CD45' in x, metals))
     not_cd45 = list(filter(lambda x: 'CD45' not in x, metals))
     not_labelled = list(filter(lambda x: '_' not in x, not_cd45))
     labelled = list(filter(lambda x: '_' in x, not_cd45))
     labels_for_live = list(filter(lambda x: ('DEAD' not in x) and ('DNA' not in x), labelled))
-    return not_cd45, not_labelled, labelled, labels_for_live
+    return cd_45, not_cd45, not_labelled, labelled, labels_for_live
+
+get_metals = get_C1_CA_metals
 
 def load_sample(ID, metals=(2, -4)):
-    stim = load_fcs(ID="Stim", file="{} Stim Cleaned.fcs".format(ID)).data
-    unstim = load_fcs(ID="Unstim", file="{} Unstim Cleaned.fcs".format(ID)).data
+    stim = load_fcs(ID="Stim", file="{} Stim Cleaned.fcs".format(ID))
+    unstim = load_fcs(ID="Unstim", file="{} Unstim Cleaned.fcs".format(ID))
     metal_names = list(stim)[metals[0]: metals[1]]
-    trans = arcsinh_trans()
-    IDstim = trans(stim.copy()[metal_names])
-    IDunstim = trans(unstim.copy()[metal_names])
+    IDstim = stim.copy()[metal_names]
+    IDunstim = unstim.copy()[metal_names]
 
     IDstim['S'] = True
     IDstim[ID] = True
